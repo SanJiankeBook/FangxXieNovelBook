cf55cebf9408337c0fb6880054843267854c3584
TaoGe <1968487008@qq.com>
2017-03-29 22:04:08 +0800

    完成了下载并解决了一些页面问题    --涛哥

--- SanJiankeBook/src/main/java/com/yc/utils/JsoupUtils.java
+++ SanJiankeBook/src/main/java/com/yc/utils/JsoupUtils.java
@@ -38,40 +38,48 @@ public class JsoupUtils{
 		StringBuilder s = new StringBuilder("");
 		List<NovelChapter> Chapter=novelChapterbiz.ShowAllChapter(nid);
 		byte[] contentInBytes=null;
+		String TextValue=null;
 		for(int i=0;i<Chapter.size();i++){
 			String con=null;
+		
 			String chapter=Chapter.get(i).getCaddress();
 			String url=chapter.substring(0,22);
-			String urll="F:\\apache-tomcat-7.0.47\\webapps\\"+chapter.substring(22); //服务器路径
+			String urll="E:\\apache-tomcat-7.0.47\\webapps\\"+chapter.substring(22); //服务器路径
 			File in = new File(urll);
 			Document doc = Jsoup.parse(in, "UTF-8", url); 
 			//Document doc = Jsoup.connect(chapter).get(); //你要抽取的地址
 			
-			Elements divs = doc.select("body");
+			Elements divs = doc.select("html");
 			
 			for (Element div: divs) { 
 		 		//System.out.println(div.select("div[class=Lyric_right]")); //你要抽取的标签<div class=Lyric_right></div>
 		 	
-		     	String content =div.select("p").toString();
+		     	String content =div.select("body").toString();
 		     	//String cte=uc.decodeUnicode(content);	  
 	           // String regEx="[`~!@#$%^&*()+=|{}';',.<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？/\"/gbody]";
 //	            String regEx = "<[^>]*>";    //去除所有的标签
 //	            Pattern p = Pattern.compile(regEx);
 //	            Matcher m = p.matcher(uc.decodeUnicode(content));
-		     	
-		     	String temp=content.replace("<p>","");
-		     	String temp1=temp.replace("</p>","\n");
-		     	con=temp1.replace("<br>","");
-		     	
+		     	content=content.replace("<body>","");
+		     	content=content.replace("</body>","");
+		     	String title=content.replace("<h1>", "");
+		     	String title1=title.replace("</h1>", "\n\r");
+		     	String temp=title1.replace("<p>","");
+		     	temp=temp.replace("&nbsp;", " ");
+		     	String temp1=temp.replace("</p>","\n\r");
+		     	con=temp1.replace("<br>","\n\r");
 	            System.out.println(con);
 	      
 //	         	con =m.replaceAll("").trim();
 //	         	con += "\n";
+	            
 	         	 			
 			}
 			//con += "\n";
-			contentInBytes = con.getBytes(); //字节流输入
+			TextValue+=con;
+            TextValue=TextValue+"\n\r";
 		}
+		contentInBytes=TextValue.getBytes();
 		return contentInBytes;
  
 	}
