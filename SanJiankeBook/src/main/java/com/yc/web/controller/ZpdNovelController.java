76dff6bf9b2d69993143afa54dd9713e737849ca
zpd <zzjj1784@163.com>
2017-03-29 20:54:24 +0800

    写完下载，但是有一个问题 --29号

--- SanJiankeBook/src/main/java/com/yc/web/controller/ZpdNovelController.java
+++ SanJiankeBook/src/main/java/com/yc/web/controller/ZpdNovelController.java
@@ -833,11 +833,6 @@ public class ZpdNovelController {
         headers.setContentDispositionFormData("attachment", fileName);   
         headers.setContentType(MediaType.TEXT_PLAIN);
         return new ResponseEntity<byte[]>(content,headers, HttpStatus.CREATED);    
-        
-		
-		
-
-
 	}
 
 }
